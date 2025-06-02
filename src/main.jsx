93b2089bbf8c90e4e235655ab80c94f097d34ab1
suphakamonLak <165622318+suphakamonLak@users.noreply.github.com>
2025-06-02 23:05:33 +0700

    add feature

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'preline/preline'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/admin/ManageOrders.jsx
+++ src/pages/admin/ManageOrders.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import TableOrders from '../../components/admin/TableOrders'
+import { Table } from "lucide-react";
+import Orders from "../../components/admin/Orders";
+import TableOrders from "../../components/admin/TableOrders";
+
 
 export default function ManageOrders() {
   return (
